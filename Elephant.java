6e292909431558b843db3c8801a73004acd150c4
William-Trudeau <349468900@gapps.yrdsb.ca>
2022-12-12 12:53:02 -0500

    1.1 Creating an Object
    I created a elephant and make it move one space each moment.

--- Elephant.java
+++ Elephant.java
@@ -1,11 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Elephant here.
- * xxx
+ * The Elephant, our hero.   
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Chan 
+ * @version 1.0.0
  */
 public class Elephant extends Actor
 {
@@ -16,5 +15,6 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
+        move(1);
     }
 }
