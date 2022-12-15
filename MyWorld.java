b668ed99dc792a8878329aa4039e7ebde1313074
William-Trudeau <349468900@gapps.yrdsb.ca>
2022-12-14 22:47:22 -0500

    1.6 Random number
    Make the apple spawn randomly in the world.

--- MyWorld.java
+++ MyWorld.java
@@ -21,7 +21,18 @@ public class MyWorld extends World
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
-        Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        createApple();
     }
-}
+    
+    /**
+     * Create a new apple at random location at top of screen.
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
+    }
+}    
+
