ea5ac60e37798d4e3a0dc6d8ab47dc1c95e24f83
William-Trudeau <349468900@gapps.yrdsb.ca>
2022-12-15 00:12:25 -0500

    2.8 Simple Timer (last commit is 2.7)
    Setup the animation speed of the elephant.

--- Elephant.java
+++ Elephant.java
@@ -14,7 +14,7 @@ public class Elephant extends Actor
     
     // Direction the elephant is facing.
     String facing = "right";
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     /**
      * Constructor - The code that gets run one time when object is created
      */
@@ -32,6 +32,9 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100, 100);
         }
+        
+        animationTimer.mark();
+        
         // Initial elephant image
         setImage(idleRight[0]);
     }
@@ -42,6 +45,11 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 120)
+        {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
