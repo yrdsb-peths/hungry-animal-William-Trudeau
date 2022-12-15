7faabdbd926dfe97c24403b05bd21c031b10699d
William-Trudeau <349468900@gapps.yrdsb.ca>
2022-12-14 23:25:53 -0500

    1.9 GameOver
    When the apple fall to the bottom of the world, It will show gameOver.

--- MyWorld.java
+++ MyWorld.java
@@ -17,7 +17,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1,false);
         
         // Create the elephant object
         Elephant elephant = new Elephant();
@@ -26,9 +26,18 @@ public class MyWorld extends World
         // Create a label
         scoreLabel = new Label(0, 80);
         addObject(scoreLabel, 50, 50);
+        
         createApple();
     }
     
+    /**
+     * Draw the game and draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
     /**
      * Increase score
      */
